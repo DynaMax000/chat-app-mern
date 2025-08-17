937128d28a08f1ebdc46cade54194ca0f736848a
Lavesh Kartik <laveshkartick1234@gmail.com>
2025-08-17 22:55:10 +0530

    Update App and Sidebar components: change theme to 'forest' in App and implement Sidebar with navigation links for Home, Friends, and Notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }
 
   return (
-    <div className="h-screen" data-theme="night">
+    <div className="h-screen" data-theme="forest">
       <Routes>
         <Route path="/" element={isAuthenticated && isOnboarded ? (<Layout showSidebar={true}> <HomePage /> </Layout>) : (<Navigate to={!isAuthenticated ? '/login' : '/onboarding'} />)
         } />
