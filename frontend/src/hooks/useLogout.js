b5507595f80219709b0846bab2995b6d9492e03b
Lavesh Kartik <laveshkartick1234@gmail.com>
2025-08-18 23:42:16 +0530

    Implement Navbar with dynamic rendering, add ThemeSelector component, and enhance logout functionality

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { logout } from '../lib/api';
+import toast from 'react-hot-toast';
+
+const useLogout = () => {
+  const queryClient = useQueryClient();
+  const { mutate:logoutMutation, isPending, error } = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['authUser']);
+      toast.success('Logged out successfully');
+    }
+  });
+
+  return { logoutMutation, isPending, error };
+};
+
+export default useLogout;
