dae7e046743e9a966d8a1a02c6537eed71b22366
Lavesh Kartik <laveshkartick1234@gmail.com>
2025-08-19 00:25:40 +0530

    Refactor PageLoader to use theme from Zustand store and update useThemeStore to persist theme in localStorage

--- frontend/store/useThemeStore.js
+++ frontend/store/useThemeStore.js
@@ -1,6 +1,9 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
 export const useThemeStore = create((set) => ({
-  theme: 'coffee',
-  setTheme: (theme) => set({ theme })
+  theme: localStorage.getItem('app-theme') || 'coffee',
+  setTheme: (theme) => {
+    localStorage.setItem('app-theme', theme);
+    set({ theme });
+  }
 }));
